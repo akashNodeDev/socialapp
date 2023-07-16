60546993c1bc61154c462e681f8aee0042e34387
akash <akash.agrawal445@gmail.com>
2023-07-16 20:55:59 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 4000 || process.env.PORT
+
+app.get("/",(req,res)=> {
+    res.send("<h1> My first program </h1>")
+})
+
+
+app.listen(PORT,()=> {
+    console.log(`Server is running at ${PORT}`)
+})
